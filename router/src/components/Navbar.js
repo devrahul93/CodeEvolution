957079d9993c7853694a54cda5e07d8cb7593b61
Rahul <r27m93@gmail.com>
2022-01-25 11:45:42 +0530

    navlink

--- router/src/components/Navbar.js
+++ router/src/components/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? 'bold' : 'normal',
+      textDecoration: isActive ? 'none' : 'underline',
+    }
+  }
+
   return (
     <nav>
       {/* link is like an anchor tag and to is like href*/}
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink style={navLinkStyles} to="/">
+        Home
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/about">
+        About
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/contact">
+        Contact
+      </NavLink>
     </nav>
   )
 }
